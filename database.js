54a2f03f9b79d2cfe7c4bc27ff531b2147e0ba63
Abby Gottlich <abby.gottlich@gmail.com>
2019-02-05 19:48:34 -0600

    can get user by id

--- database.js
+++ database.js
@@ -1,9 +1,18 @@
 let fs = require("fs");
 
-exports.create = function(type,data,doneCallback){
-  
-}
-exports.find = function(type,id,doneCallback){
+exports.create = function (type, data, doneCallback) {
 
+}
+exports.find = function (type, id, doneCallback) {
+    // type = "users"
+    fs.readFile("db.json", (err, data) => {
+
+        let allTheStuff = JSON.parse(data);
+        // allTheStuff = {}
+        let users = allTheStuff[type];
+        let user = users.find(u => u.id === id);
+        doneCallback(user);
+
+    })
 }
 
--- index.js
+++ index.js
@@ -2,35 +2,39 @@
 var http = require('http');
 let server = http.createServer(messageReceived);
 server.listen(8080);
+let { find, create } = require("./database");
 
 
 function messageReceived(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    if(req.method === "GET" && req.url === "/users"){
+  if (req.method === "GET" && req.url === "/users") {
+    res.end();
+  }
+  else if (req.method === "GET" && req.url.indexOf("/users/") > -1) {
+    let id = req.url.split("/");
+    id = Number(id[2]);
+    find("users", id, (user) => {
+      res.write(JSON.stringify(user));
       res.end();
-    }
-    else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
-      let id = req.url.split("/");
-      id = Number(id[2]);
+    })
+  }
+  else if (req.method === "POST" && req.url === "/users") {
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      let user = JSON.parse(body);
+      user._id = users.length + 1;
+
       res.end();
-    }
-    else if(req.method === "POST" && req.url === "/users"){
-        let body = [];
-        req.on('data', (chunk) => {
-          body.push(chunk);
-        }).on('end', () => {
-          body = Buffer.concat(body).toString();
-          let user = JSON.parse(body);
-          user._id = users.length + 1;
-         
-          res.end();
-        });
-    }
-    else{
-     res.write("Not Found");
-     res.end();
-    }
-    
+    });
+  }
+  else {
+    res.write("Not Found");
+    res.end();
+  }
+
 }
 
