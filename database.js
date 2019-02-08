78d6ca5e5ff1b1da98272c346abd568967ec13fa
Abby Gottlich <abby.gottlich@gmail.com>
2019-02-07 18:46:46 -0600

    create function done

--- database.js
+++ database.js
@@ -1,15 +1,17 @@
 let fs = require("fs");
 
 exports.create = function (type, data, doneCallback) {
-    fs.writeFile("db.json", (err, data) => {
+    fs.readFile("db.json", (err, data) => {
 
+        // parse to turn data into an object
         let allTheStuff = JSON.parse(data);
-        // allTheStuff = {}
-        let users = allTheStuff[type];
-        let user = users.push(data);
-        JSON.stringify(data);
-        doneCallback(data);
-
+        // turn into an array and manipulate the array
+        allTheStuff[type].push(data);
+        // turn it back into a string
+        let thingsAsString = JSON.stringify(allTheStuff);
+        fs.writeFile("db.json", thingsAsString, function () {
+            doneCallback(user);
+        })
     })
 }
 exports.find = function (type, id, doneCallback) {
